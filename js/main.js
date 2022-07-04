557b0c8013b56de450ea9f455cd6ed8d55aaff75
vtnboolean <vnthuanx@gmail.com>
2022-07-04 17:15:51 +0200

    first commit

---
+++ js/main.js
@@ -0,0 +1,10 @@
+const app = new Vue (
+    {
+        el: '#root',
+        data: {
+            "message": "Hello World!",
+            imgSource: "https://www.coresecurity.com/sites/default/files/inline-images/pen-test.png"
+        }
+
+    }
+);
